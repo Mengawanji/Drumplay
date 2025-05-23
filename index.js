c8a1bc2ba809cfc71bc114ff47eba1d8fdf3db97
Menga wanji <martialwanji@gmail.com>
2025-05-23 23:08:30 +0100

    Adding sounds

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
-let keys = document.getElementsByClassName("drum");
+var sound = new Audio('sounds/crash.mp3');
+var keys = document.getElementsByClassName("drum");
+
 
 for (let i = 0; i < keys.length; i++) {
 
     keys[i].addEventListener("click", play)
 
     function play() {
-        alert("Play the drum");
+        sound.play();
     }
 }
+
 
