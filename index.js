d8e0ed59623858f97184eaf97b72ec72c143c2d6
Menga wanji <martialwanji@gmail.com>
2025-05-25 19:44:53 +0100

    Adding Key-touch and Animations

--- index.js
+++ index.js
@@ -1,15 +1,121 @@
 
-var sound = new Audio('sounds/crash.mp3');
-var keys = document.getElementsByClassName("drum");
+
+//Detecting button press
+
+var noButtons = document.querySelectorAll(".drum");
 
 
-for (let i = 0; i < keys.length; i++) {
-  
-    keys[i].addEventListener("click", play)
+for (let i = 0; i < noButtons.length; i++) {
+    noButtons[i].addEventListener("click", function() {
+        
+        var btnInnerHTML= this.innerHTML;
+        makeSound(btnInnerHTML);
+        btnAnimation(btnInnerHTML);
+          
 
-    function play() {
-        sound.play();
-    }
+    });
+
+}
+
+//Detecting keyboard press
+
+document.addEventListener("keypress",function (event){
+    makeSound(event.key);
+    btnAnimation(event.key);
+} ) ;
+
+
+
+function makeSound(key) {
+    switch (key) {
+            
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+    
+            break;
+            case "a":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                    kick.play();
+                break;
+                case "s":
+                    var snare = new Audio('sounds/snare.mp3');
+                        snare.play();
+                    break;
+                    case "d":
+                        var tom1 = new Audio('sounds/tom-1.mp3');
+                            tom1.play();
+                        break;
+                        case "j":
+                            var tom2 = new Audio('sounds/tom-2.mp3');
+                                tom2.play();
+                            break;
+                            case "k":
+                                var tom3 = new Audio('sounds/tom-3.mp3');
+                                    tom3.play();
+                                break;  
+                                case "l":
+                                    var tom4 = new Audio('sounds/tom-4.mp3');
+                                        tom4.play();
+                                    break;          
+    
+        default:
+            console.log(btnInnerHTML);
+    }  
+}
+
+//Adding button animations
+
+function btnAnimation(currentKey) {
+    var activeBtn = document.querySelector("."+ currentKey);
+    activeBtn.classList.add("pressed"); 
+
+    setTimeout (function() {
+        activeBtn.classList.remove("pressed"); 
+    }, 250);
+
 }
 
 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+// var sound = new Audio('sounds/crash.mp3');
+// var keys = document.getElementsByClassName("drum");
+// var a = keys.length
+
+
+// for (let i = 0; i < keys.length; i++) {
+//     keys[i].addEventListener("click", play);
+  
+
+//     function play () {
+//         if ( a = 0){
+//             var crash = new Audio('sounds/crash.mp3');
+//                 crash.play();
+//         }else if( a = 1){
+//             var kick = new Audio('sounds/kick-bass.mp3');
+//                 kick.play();
+
+//         }else {
+//             var tom4 = new Audio('sounds/tom4.mp3');
+//                 tom4.play();
+//         }
+//     }
+// } 
